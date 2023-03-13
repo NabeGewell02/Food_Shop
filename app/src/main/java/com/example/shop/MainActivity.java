06b31fad35c6c2530295b6841b8a538cdf64e5f0
angelina <gorgzisg@gmail.com>
2023-03-13 16:58:28 +0300

    initial commit

---
+++ app/src/main/java/com/example/shop/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    int kol_vo = 0;
+    Spinner spinner;
+    ArrayList spinnerArrayList;
+    ArrayAdapter spinnerAdapter;
+    HashMap goodsMap;
+    String goodsName;
+    double cena;
+    double stoimost;
+    ImageView kartinka;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CreateSpinner();
+        kartinka = findViewById(R.id.imageView4);
+        stoimost = 0;
+        CreateMap();
+    }
+    void CreateSpinner(){
+        spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        spinnerArrayList = new ArrayList();
+        spinnerArrayList.add("Pizza");
+        spinnerArrayList.add("Sushi");
+        spinnerArrayList.add("Wok");
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+
+    }
+    void CreateMap(){
+        goodsMap = new HashMap();
+        goodsMap.put("Pizza", 600.00);
+        goodsMap.put("Sushi", 900.00);
+        goodsMap.put("Wok", 500.00);
+    }
+
+    public void priplusovat(View view) {
+        kol_vo = kol_vo + 1;
+        TextView Kol_vo_predmetov1 = findViewById(R.id.textView6);
+        TextView stoimost_predmetov1 = findViewById(R.id.textView5);
+        stoimost_predmetov1.setText("" + cena * kol_vo + " рублей");
+        Kol_vo_predmetov1.setText("" + kol_vo);
+    }
+
+    public void otnyat(View view) {
+        kol_vo = kol_vo - 1;
+
+        TextView Kol_vo_predmetov2 = findViewById(R.id.textView6);
+        TextView stoimost_predmetov2 = findViewById(R.id.textView5);
+        if (stoimost > 0 & kol_vo == 0)
+        {
+            Kol_vo_predmetov2.setText("" + kol_vo);
+            stoimost_predmetov2.setText("" + cena * Math.abs(kol_vo) + " рублей");
+        }
+        else if (kol_vo == 0 & stoimost == 0) {
+            kol_vo = 0;
+            Kol_vo_predmetov2.setText("" + kol_vo);
+            stoimost_predmetov2.setText("-" + cena * kol_vo + " рублей");
+        }
+        else{
+            Kol_vo_predmetov2.setText("" + kol_vo);
+            stoimost_predmetov2.setText("" + cena * kol_vo + " рублей");
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        goodsName = spinner.getSelectedItem().toString();
+        cena = (double)goodsMap.get(goodsName);
+        /*if (goodsName.equals("Pizza"))
+        {
+            kartinka.setImageResource(R.drawable.img_1);
+        }
+        else if (goodsName.equals("Sushi"))
+        {
+            kartinka.setImageResource(R.drawable.img);
+        }
+        else if (goodsName.equals("Wok"))
+        {
+            kartinka.setImageResource(R.drawable.wok);
+        }*/
+        switch(goodsName)
+        {
+            case "Sushi":
+                kartinka.setImageResource(R.drawable.img);
+                break;
+            case "Wok":
+                kartinka.setImageResource(R.drawable.wok);
+                break;
+            default:
+                kartinka.setImageResource(R.drawable.pizza);
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void Dobavit(View view) {
+        TextView zakaz_stoimost = findViewById(R.id.Obshaya_stoimost);
+        stoimost += cena * kol_vo;
+        zakaz_stoimost.setText("Общая стоимость заказа: " + stoimost + " рублей");
+        cena = 0;
+        TextView cena1 = findViewById(R.id.textView5);
+        cena1.setText("" + cena + " рублей");
+        kol_vo = 0;
+        TextView kol_vo1 = findViewById(R.id.textView6);
+        kol_vo1.setText("" + kol_vo);
+    }
+}
